cced138b99dc653ddae562c98a55faa43bb62898
Oncomingdarkness <76438406+Oncomingdarkness@users.noreply.github.com>
2021-12-11 15:59:33 +0000

    Update and rename Runtoday.py to Run.py

---
+++ Scripts/Run.py
@@ -0,0 +1,13 @@
+import pluNNedFunc
+from pluNNedFunc import PLUNNED
+#PLUNNED('512x256x128correct.pt','./trajectories/shifted_scaled_13.xyz',plumedfile='plumedtoday.dat',colvarname='COLVARtoday',run='yes')
+#PLUNNED('8x4x2.pt','./trajectories/shifted_scaled_13.xyz',plumedfile='plumedtoday.dat',colvarname='COLVARtoday',run='yes')
+#PLUNNED('8x4x2.pt','./trajectories/dump.3Dcolloid.13.xyz',plumedfile='plumedtoday.dat',colvarname='COLVARtoday',run='yes')
+#512x256x128correct.pt
+#shifted_scaled_13.xyz
+#PLUNNED('512x256x128correct.pt','./trajectories/shifted_scaled_13change.xyz',plumedfile='plumedtoday.dat',colvarname='COLVARtoday',run='yes')
+#PLUNNED('8x4x2.pt','./trajectories/shifted_scaled_Trunc_13change.xyz',plumedfile='plumedtoday.dat',colvarname='COLVARtoday',run='yes')
+#shifted_scaled_Trunc_13change.xyz
+#PLUNNED('512x256x128correct.pt','./trajectories/shifted_scaled_Trunc_13change.xyz',plumedfile='plumedtoday.dat',colvarname='COLVARtoday',run='yes')
+#PLUNNED('../512x256x128correct.pt','../trajectories/shifted_scaled_13Sing.xyz',plumedfile='plumedsing.dat',colvarname='COLVARsing',run='yes')
+PLUNNED('../512x256x128correct.pt','../trajectories/dump.3Dcolloid.13_DivShift.xyz',plumedfile='All.dat',colvarname='All',run='yes')
